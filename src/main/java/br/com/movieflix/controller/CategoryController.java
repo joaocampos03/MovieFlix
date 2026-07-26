952847a1d5186d274f4aed32ee112b09b4403b8b
João Campos <joaomarcelo_1000@hotmail.com>
2026-07-26 01:53:49 -0300

    first commit

---
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -0,0 +1,51 @@
+package br.com.movieflix.controller;
+
+import br.com.movieflix.controller.request.CategoryRequest;
+import br.com.movieflix.controller.response.CategoryResponse;
+import br.com.movieflix.entity.Category;
+import br.com.movieflix.mapper.CategoryMapper;
+import br.com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<CategoryResponse> categories = categoryService.findAll()
+                .stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList();
+
+        return ResponseEntity.ok(categories);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest request) {
+        Category newcategory = CategoryMapper.toCategory(request);
+        Category savedCategory = categoryService.saveCategory(newcategory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CategoryMapper.toCategoryResponse(savedCategory));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> getByCategoryId(@PathVariable Long id) {
+        return categoryService.findById(id)
+                .map(category -> ResponseEntity.ok(CategoryMapper.toCategoryResponse(category)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteByCategoryId(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+}

689e374225fa9616c24bd82032d14875ce2db6b0
João Campos <joaomarcelo_1000@hotmail.com>
2026-07-31 00:53:13 -0300

    feat: Implementa cadastro de filmes.

---
+++ src/main/java/br/com/movieflix/controller/MovieController.java
@@ -0,0 +1,33 @@
+package br.com.movieflix.controller;
+
+import br.com.movieflix.controller.request.MovieRequest;
+import br.com.movieflix.controller.response.MovieResponse;
+import br.com.movieflix.entity.Movie;
+import br.com.movieflix.mapper.MovieMapper;
+import br.com.movieflix.service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movieflix/movie")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest request) {
+        Movie savedMovie = movieService.save(MovieMapper.toMovie(request));
+        return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll() {
+        return ResponseEntity.ok(movieService.findAll().stream()
+                .map(movie -> MovieMapper.toMovieResponse(movie))
+                .toList());
+    }
+}
---
+++ src/main/java/br/com/movieflix/service/MovieService.java
@@ -0,0 +1,24 @@
+package br.com.movieflix.service;
+
+import br.com.movieflix.entity.Movie;
+import br.com.movieflix.repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.annotation.RequiredTypes;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MovieService {
+
+    private final MovieRepository movieRepository;
+
+    public Movie save(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    public List<Movie> findAll() {
+        return movieRepository.findAll();
+    }
+}

194ab7b94c4ea57260fa0cbbbd1c3d4a62f3040b
João Campos <joaomarcelo_1000@hotmail.com>
2026-08-08 02:07:38 -0300

    feature: Implementa security token.

--- src/main/java/br/com/movieflix/TokenService.java
+++ src/main/java/br/com/movieflix/TokenService.java
@@ -1,13 +1,16 @@
 package br.com.movieflix;
 
+import br.com.movieflix.config.JWTUserData;
 import br.com.movieflix.entity.User;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
 import java.time.Instant;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -26,5 +29,24 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("API Movieflix")
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserData> verifyToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+
+            DecodedJWT jwt = JWT.require(algorithm)
+                    .build()
+                    .verify(token);
+
+            return Optional.of(JWTUserData
+                    .builder()
+                    .id(jwt.getClaim("userId").asLong())
+                    .name(jwt.getClaim("name").asString())
+                    .email(jwt.getSubject())
+                    .build());
+        } catch (JWTVerificationException ex) {
+            return Optional.empty();
+        }
     }
 }
